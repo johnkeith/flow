f20da2f94a68eb447e672a6b27faebf9533bfff6
John Keith <johnkeith@outlook.com>
2018-01-28 15:53:14 -0500

    more reorg

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -5,6 +5,6 @@ import HelloAPI from './components/HelloAPI'
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <HelloAPI />,
-    document.body.appendChild(document.createElement('div')),
+    document.getElementById('app'),
   )
 })
