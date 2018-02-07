61b44a25152b2130a5568de46f1c74e89ba2638c
John Keith <johnkeith@outlook.com>
2018-02-06 20:39:29 -0500

    reorg of javascripts folder

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,20 @@
 const { environment } = require('@rails/webpacker')
 
+const babelLoader = environment.loaders.get('babel')
+
+environment.loaders.insert('svg', {
+  test: /\.svg$/,
+  use: babelLoader.use.concat([
+    {
+      loader: 'react-svg-loader',
+      options: {
+        jsx: true // true outputs JSX tags
+      }
+    }
+  ])
+}, { after: 'file' })
+
+const fileLoader = environment.loaders.get('file')
+fileLoader.exclude = /\.(svg)$/i
+
 module.exports = environment
