568cd06e1927343e8d1ce8421d0a9e2ec83e2006
Ben Butler <benbutler08@gmail.com>
2018-02-09 08:53:56 -0500

    Spelling
    correct survey_responses spelling

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,5 +1,5 @@
 class Survey < ApplicationRecord
   belongs_to :account
-  has_many :survey_reponses, dependent: :destroy
-  has_many :team_members, through: :survey_reponses
+  has_many :survey_responses, dependent: :destroy
+  has_many :team_members, through: :survey_responses
 end
--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,5 +1,5 @@
 class TeamMember < ApplicationRecord
   belongs_to :team
-  has_many :survey_reponses
-  has_many :surveys, through: :survey_reponses
+  has_many :survey_responses
+  has_many :surveys, through: :survey_responses
 end
