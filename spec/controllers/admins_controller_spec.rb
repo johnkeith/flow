3ac6698687df63ff98b31e02dc9f594f3d0b9700
Ben Butler <benbutler08@gmail.com>
2018-02-14 15:12:57 -0500

    response expectations fixed
    Change the response expectations. passing.

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -3,18 +3,15 @@ require "rails_helper"
 RSpec.describe Api::V1::AdminsController, type: :controller do
   describe 'GET #index' do
   admin = FactoryBot.create(:admin)
-  subject { get :index }
-    it { is_expected.to have_http_status(:ok) }
-    it 'will return records' do
-      expect(JSON.parse(subject.body)).to eql(
-        [{
-         "id"=> admin.id,
-         "name"=> "test admin",
-         "account_id"=> admin.account_id ,
-         "created_at"=> Time.current,
-         "updated_at"=> Time.current,
-        }])
+  it "returns all the admins" do
+    get :index
+      admins = JSON.parse(response.body)
+      expect(response.status).to eq 200
+      expect(admin["name"]).to eq admin.name
+      expect(admin["id"]).to eq admin.id
+      expect(admin["account_id"]).to eq admin.account_id
+      expect(admin["created_at"]).to eq admin.created_at
+      expect(admin["updated_at"]).to eq admin.updated_at
     end
   end
-
 end
