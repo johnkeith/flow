9037267bcab5363bc27d6064e6be0c04d1a3b0d6
Ben Butler <benbutler08@gmail.com>
2018-02-14 11:12:53 -0500

    add validation test
    test that an admin must have a name to be saved

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -6,10 +6,19 @@ RSpec.describe Api::V1::AdminsController, type: :controller do
   let(:admin) { build(:admin) }
   subject { get :index }
 
-  it { is_expected.to have_http_status(:ok) }
+    it { is_expected.to have_http_status(:ok) }
 
     it 'will return records' do
       expect(JSON.parse(subject.body)).to eql([])
     end
   end
+
+  context 'admin name validation' do
+    let(:admin) { build(:admin) }
+      it 'ensures only admins with names are saved' do
+      admin.name = nil
+      expect(admin.save).to eql(false)
+    end
+  end
+
 end
