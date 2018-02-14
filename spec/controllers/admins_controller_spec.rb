1306f5e9690c92257000761037a325e11b3a22b6
Ben Butler <benbutler08@gmail.com>
2018-02-14 12:59:19 -0500

    show action
    create a test for the show action

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe Api::V1::AdminsController, type: :controller do
     end
   end
 
+  describe 'GET #show' do
+    admin = FactoryBot.build(:admin)
+    subject { get :show, id: admin.id }
+    it { is_expected.to have_http_status(:ok) }
+  end
+
   context 'admin name validation' do
     let(:admin) { build(:admin) }
       it 'ensures only admins with names are saved' do
