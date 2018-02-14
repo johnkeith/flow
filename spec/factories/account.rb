31797a089e92872141964a0a082660edf3ca8dfd
Ben Butler <benbutler08@gmail.com>
2018-02-14 09:34:53 -0500

    account factory
    add an account factory and an association in the admins factory. Change factory names to singular.

---
+++ spec/factories/account.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :account do
+    name 'Test Account'
+    id '1'
+  end
+end
--- spec/factories/admin.rb
+++ spec/factories/admin.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :admin do
     id 1
-    name "test admin"
+    name 'test admin'
+    account
+    account_id '1'
   end
 end
