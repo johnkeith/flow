aed47f6590b4cd2eded759dee7ea0e168c3b5722
Ben Butler <benbutler08@gmail.com>
2018-02-15 11:35:24 -0500

    change index comparison
    Change factoryBot call to create_list, check for length of the list created. passing.

--- spec/requests/admins_controller_spec.rb
+++ spec/requests/admins_controller_spec.rb
@@ -1,13 +1,16 @@
-require "rails_helper"
 
 RSpec.describe Api::V1::AdminsController, type: :controller do
   describe 'GET #index' do
-  admin = FactoryBot.create(:admin)
+  it 'returns successfully' do
+    get :index, format: :json
+    expect(response.status).to eq 200
+  end
+
   it "returns all the admins" do
-   get :index
+   admin = FactoryBot.create_list(:admin, 10)
+   get :index, format: :json
       json = JSON.parse(response.body)
-      expect(response.status).to eq 200
-      expect(json.length).to eql(Admin.last.id)
+      expect(json.length).to eql(10)
     end
   end
 end
