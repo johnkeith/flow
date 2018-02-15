387fd1733864d21a33dcc424a92aedc65d3b38f1
Ben Butler <benbutler08@gmail.com>
2018-02-15 17:51:16 -0500

    dry up the spec
    collect Factories to the top, set before hooks

--- spec/requests/admins_controller_spec.rb
+++ spec/requests/admins_controller_spec.rb
@@ -1,25 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::AdminsController, type: :controller do
+  # initialize test data
+  let!(:admin_list) { create_list(:admin, 10) }
+  let!(:account) { create(:account) }
+
+
   describe 'GET #index' do
+    before { get :index }
   it 'returns successfully' do
-    get :index, format: :json
     expect(response.status).to eq 200
   end
 
   it "returns all the admins" do
-   FactoryBot.create_list(:admin, 10)
-   get :index
       json = JSON.parse(response.body)
       expect(json.length).to eql(10)
     end
   end
 
   describe 'GET #show' do
+    before { FactoryBot.create(:admin, id: 1) }
+    before { get :show, params: { id: 1 } }
     it 'returns successfully' do
-      FactoryBot.create(:admin, id: 1)
-      get :show, params: {id: 1}
-      expect(response.status).to eq 200
+      expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body)).to_not be_nil
     end
   end
@@ -27,13 +30,12 @@ RSpec.describe Api::V1::AdminsController, type: :controller do
   describe 'POST #create' do
     context 'admin is created' do
       it 'returns successfully' do
-        FactoryBot.create(:account)
         post :create, params: FactoryBot.attributes_for(:admin, :account_id => Account.first.id)
         expect(response).to have_http_status(:ok)
       end
     end
 
-    context 'admin is not created' do
+    context 'admin creation is invalidated' do
       it 'raises an ActiveRecord error' do
         expect {
           post :create, params: FactoryBot.attributes_for(:admin)
@@ -41,4 +43,6 @@ RSpec.describe Api::V1::AdminsController, type: :controller do
       end
     end
   end
+
+
 end
