35c592823776c10c0163f9357833dbb4a8f5da62
Ben Butler <benbutler08@gmail.com>
2018-02-16 09:16:14 -0500

    add faker and seeds
    Seed the database with faked records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+Account.create(id: 100, name: Faker::Name.name)
+
+
+5.times do
+  Admin.create(name: Faker::Name.name, account_id: 100)
+end
