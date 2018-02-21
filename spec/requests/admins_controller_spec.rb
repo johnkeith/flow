6ae60ca46a9663de0f5c631664de334d1bf79379
Ben Butler <benbutler08@gmail.com>
2018-02-21 09:08:41 -0500

    destroy test
    Add test for destroy action. Passing.

--- spec/requests/admins_controller_spec.rb
+++ spec/requests/admins_controller_spec.rb
@@ -13,21 +13,21 @@ RSpec.describe Api::V1::AdminsController, type: :controller do
     before do
       get :index
     end
-    
+
     it 'returns successfully' do
       expect(response.status).to eq 200
     end
 
     it "returns all the admins" do
       json = JSON.parse(response.body)
-      
+
       expect(json.length).to eql(Admin.all.length)
     end
   end
 
   context 'GET #show' do
-    before do 
-      get :show, params: { id: admin.id } 
+    before do
+      get :show, params: { id: admin.id }
     end
 
     it 'returns successfully' do
@@ -37,14 +37,14 @@ RSpec.describe Api::V1::AdminsController, type: :controller do
   end
 
   context 'POST #create' do
-    it 'returns successfully' do        
-      post :create, params: { 
-        admin: { 
+    it 'returns successfully' do
+      post :create, params: {
+        admin: {
           name: 'Bill Murray',
-          account_id: account.id 
-        } 
+          account_id: account.id
+        }
       }, format: :json
-      
+
       expect(response).to have_http_status(:ok)
     end
 
@@ -59,14 +59,23 @@ RSpec.describe Api::V1::AdminsController, type: :controller do
 
   describe 'PUT #update' do
     it 'should update successfully' do
-      put :update, params: { 
+      put :update, params: {
         id: admin.id,
         admin: {
-          name: 'changed by update' 
-        } 
+          name: 'changed by update'
+        }
       }, format: :json
-      
+
       expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'should delete the admin' do
+      delete :destroy, params: {
+        id: admin.id
+      }, format: :json
+      expect(response.status).to eq 204
     end
   end
 end
