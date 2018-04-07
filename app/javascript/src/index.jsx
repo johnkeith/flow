154a9a70f53db08878dd65526964b2922ce60da0
John Keith <johnkeith@outlook.com>
2018-04-07 14:13:34 -0400

    cleanup

--- app/javascript/src/index.jsx
+++ app/javascript/src/index.jsx
@@ -4,7 +4,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Hello from './components/hello'
+import Hello from './components/_hello'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
